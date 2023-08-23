969ad98958fc17bdd9e2fed6ef9f1505b544f29a
moliyy0402 <2487580445@qq.com>
2023-08-23 09:20:10 +0800

    添加项目文件。

---
+++ 基础语法/基础语法/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 基础语法
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("666");
+
+            Console.ReadKey();
+        }
+    }
+}
