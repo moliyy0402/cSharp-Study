ae89ee25a3c40d133bbdae554c0dd608e4570834
moliyy0402 <2487580445@qq.com>
2023-08-24 11:14:08 +0800

    函数

---
+++ 基础语法/基础语法/Func.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 基础语法
+{
+    internal class Func
+    {
+        public static int FindMax(int num1, int num2)
+        {
+            int result;
+
+            if (num1 > num2)
+            {
+                result = num1;
+            }
+            else
+            {
+                result = num2;
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// 递归求阶乘
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int Factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return Factorial(num - 1) * num;
+            }
+
+        }
+
+        /// <summary>
+        /// 交换2个数的值
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        public static void swap(int num1, int num2) {
+            int temp = num1;
+
+            num1 = num2;
+
+            num2 = temp;
+
+
+            Console.WriteLine("在交换之中，a 的值： {0}", num1);
+            Console.WriteLine("在交换之中，b 的值： {0}", num2);
+
+        }
+
+        /// <summary>
+        /// 交换两个数据的值
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        public static void swap2(ref int num1,ref int num2)
+        {
+            int temp = num1;
+
+            num1 = num2;
+
+            num2 = temp;
+
+
+            Console.WriteLine("在交换之中，a 的值： {0}", num1);
+            Console.WriteLine("在交换之中，b 的值： {0}", num2);
+
+        }
+
+        /// <summary>
+        /// 输出参数
+        /// </summary>
+        /// <param name="x"></param>
+        public static void getValue(out int x)
+        {
+            x = 666;
+        }
+
+        /// <summary>
+        /// 输出参数
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public static void getValue(out int x,out int y)
+        {
+            x = 666;
+            y = 999;
+        }
+    }
+}
--- 基础语法/基础语法/Program.cs
+++ 基础语法/基础语法/Program.cs
@@ -30,15 +30,57 @@ namespace 基础语法
             //e1.width = 1.5;
             //e1.Display();
 
+            //Encapsulation2 e2 = new Encapsulation2();
+            //e2.setVauleForKeys();
+            //e2.Display();
+            //Console.ReadKey();
 
-            Encapsulation2 e2 = new Encapsulation2();
-            e2.setVauleForKeys();
+            //int r = Func.FindMax(3, 4);
+            //Console.WriteLine(r);
 
-            e2.Display();
+            //int n = 5;
+            //Console.WriteLine("{0}的阶乘是：{1}", n, Func.Factorial(n));
 
+
+            /* 局部变量定义 */
+            //int a = 100;
+            //int b = 200;
+
+            //Console.WriteLine("在交换之前，a 的值： {0}", a);
+            //Console.WriteLine("在交换之前，b 的值： {0}", b);
+
+            ///* 调用函数来交换值 */
+            //// Func.swap(a, b);
+
+            //Func.swap2(ref a,ref b);
+
+            //Console.WriteLine("在交换之后，a 的值： {0}", a);
+            //Console.WriteLine("在交换之后，b 的值： {0}", b);
+
+
+
+
+            /* 局部变量定义 */
+            int a = 100;
+
+            Console.WriteLine("在方法调用之前，a 的值： {0}", a);
+
+            /* 调用函数来获取值 */
+            Func.getValue(out a);
+
+            Console.WriteLine("在方法调用之后，a 的值： {0}", a);
+
+
+
+            /* 局部变量定义 */
+            int q, w;
+
+            /* 调用函数来获取值 */
+            Func.getValue(out q, out w);
+
+            Console.WriteLine("在方法调用之后，q 的值： {0}", q);
+            Console.WriteLine("在方法调用之后，w 的值： {0}", w);
             Console.ReadKey();
-
-
         }
     }
 }
